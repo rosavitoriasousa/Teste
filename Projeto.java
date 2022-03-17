a10a380bc8c9e747fc41f7f26a7e8b2d79064007
rosavitoriasousa <101807207+rosavitoriasousa@users.noreply.github.com>
2022-03-17 10:01:40 -0300

    Add files via upload

---
+++ Projeto.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2etimds
+ */
+public class Projeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Digite um número: ");
+        Scanner numero= new Scanner(System.in);
+        int num1 = numero.nextInt();
+        
+        int result = 0;
+        
+        for(int i = 0; i <= num1; i++){
+            result += i;
+        }
+        System.out.println("Resultado: ");
+        System.out.println(result);
+        
+        }
+    }
