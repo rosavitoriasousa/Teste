526ed0f289ee53ab7321ffb98df69ea721f9c5ea
rosavitoriasousa <101807207+rosavitoriasousa@users.noreply.github.com>
2022-03-18 12:10:25 -0300

    Update and rename Projeto2.java to Projeto3.java

---
+++ Projeto3.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+/**
+ *
+ * @author 2etimds
+ */
+public class Projeto2 {
+    
+    public static void main(String[] args){
+        
+    /* Unidade = um, dois, três, quatro, cinco, seis, sete, oito,
+    nove
+
+    Dezena = dez, vinte, trinta, quarenta, cinquenta, sessenta, setenta,
+    oitenta, noventa
+
+    Centena = cem, duzentos, trezentos, quatrocentos, quinhentos,
+    seiscentos, setecentos, oitcentos, novecentos
+
+    Outros = , onze, doze, treze, catorze, quinze, dezesseis, dezessete,
+    dezoito, dezenove
+
+    se o número digitado tiver um caracter, leia a unidade
+    mas se tiver dois caracteres, leia unidade e dezena ou outros
+    mas se tiver três caracteres, leia unidade, dezena e centena
+    se não valor inváliudo
+
+    int 1 = unidade
+    int 2 = dezena + unidade
+    int 3 = centena + dezena + unidade
+
+    Numéro digitado = num
+
+    if(num = 1){
+        escreva unidade
+    }
+    else if(num = 2){
+        escreva dezena e unidade
+    }
+    else if(num = 3){
+        escreva centena, dezena e unidade
+    }
+    else{
+        Valor inválido
+    } */
+        
+    }
+}
