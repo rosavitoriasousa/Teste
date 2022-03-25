d1889214478c5ea3f64cfe301fe6158f3d792d04
rosavitoriasousa <101807207+rosavitoriasousa@users.noreply.github.com>
2022-03-25 19:25:02 -0300

    Update Projeto1.java

--- Projeto1.java
+++ Projeto1.java
@@ -5,10 +5,6 @@ Scanner leitor = new Scanner(System.in);
 System.out.println("Digite um valor");
 String nuns = leitor.nextLine();
 
-
-
-
-
 int unidade = Integer.parseInt(String.valueOf(nuns.charAt(0)));
 //System.out.println(unidade);
 int dezena = Integer.parseInt(String.valueOf(nuns.charAt(0)));
@@ -16,18 +12,10 @@ int dezena = Integer.parseInt(String.valueOf(nuns.charAt(0)));
 int centena = Integer.parseInt(String.valueOf(nuns.charAt(0)));
 //System.out.println(centena);
 
-
-
-
-
 String[] uni = {"zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"};
 String[] deze = {"dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezessete",
 "dezoito", "dezenove", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
 String[] cen = {"cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitcentos", "novecentos"};
-
-
-
-
 
 System.out.println("Número: " + uni[unidade]);
 System.out.println("Número: " + deze[dezena]);
