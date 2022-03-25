e18f307f7a3d4cc5522bd3c10259983bf5da2402
rosavitoriasousa <101807207+rosavitoriasousa@users.noreply.github.com>
2022-03-25 10:49:07 -0300

    Update Projeto3.java

--- Projeto3.java
+++ Projeto3.java
@@ -19,15 +19,22 @@ public class Projeto1 {
         // 123
         int unidade = Integer.parseInt(String.valueOf(nuns.charAt(0)));
         System.out.println(unidade);
-        int dezena = Integer.parseInt(String.valueOf(nuns.charAt(1)));
+        int dezena = Integer.parseInt(String.valueOf(nuns.charAt(0)));
         System.out.println(dezena);
+        int centena = Integer.parseInt(String.valueOf(nuns.charAt(0)));
+        System.out.println(centena);
         
         String[] uni = {"zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"};
         String[] deze = {"dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezessete", 
         "dezoito", "dezenove", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
         String[] cen = {"cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitcentos", "novecentos"};
-        System.out.println("Número: " + uni[unidade]);
-         System.out.println("Número: " + deze[dezena]);
-       
+        
+        if(nuns = uni){
+            System.out.println("Número: " + uni[unidade]);
+        } 
+        
+        /* System.out.println("Número: " + uni[unidade]);
+        System.out.println("Número: " + deze[dezena]);
+        System.out.println("Número: " + cen[centena]); */
     }
 }
