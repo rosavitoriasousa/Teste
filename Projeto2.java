d660cef13f460d30a523ef41adffd32f3af2ec50
Roferoro <101807154+Roferoro@users.noreply.github.com>
2022-03-25 18:57:12 -0300

    Update Projeto2.java

--- Projeto2.java
+++ Projeto2.java
@@ -1,34 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package projeto;
-
-/**
- *
- * @author 2etimds
- */
-public class Projeto2 {
-    
-    public static void main(String[] args){
+import java.util.Scanner;
+public class MyClass {
+        /**
+ * @param args os argumentos da linha de comando
+     */
+ public static void main(String[] args) {
+Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite um valor:");
         
-        int dig = 0;
-        int num = dig;
-        int result = num;
-        int i = 1;
-
-        System.out.println("Digite um valor.");
-
+        int num = leitor.nextInt(); // Pega o primeiro valor digitado
+        float result = num; // Define o valor inicial do resultado
+        int quantidadeNumeros = 1; // Variável que armazena a quantidade de numeros
+        
         while(num > 0){
-            System.out.println("Digite outro valor.");
-
-            num = dig;
-
-            if(num > 0){
-                result += num;
-                i++;
-            }
+            System.out.println("Digite outro valor");
+            num = leitor.nextInt(); // Atualiza o valor com o novo valor digitado
+            if(num < 0) break; // Se o valor digitado for negativo QUEBRA o Loop
+            result += num; // Soma o valor guardado com o valor digitado
+            quantidadeNumeros++; // Adiciona 1 na quantidade de numeros digitados
         }
-    }
+        System.out.println("A media foi: " + (result / quantidadeNumeros)); // Faz a formula e Printa o resultado
+}
 }
